1d2f049170cccb3c1f9114de8786c0711a2674cd
STEFANO LAZZARI <stefanolazzari79@libero.it>
2021-04-20 23:07:16 +0200

    --

--- src/pages/Videocall.vue
+++ src/pages/Videocall.vue
@@ -1,46 +1,55 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 my-3">
-        <h2>Room</h2>
-        <input v-model="roomId">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="">
-          <vue-webrtc ref="webrtc"
-                      width="100%"
-                      :roomId="roomId"
-                      v-on:joined-room="logEvent"
-                      v-on:left-room="logEvent"
-                      v-on:opened-room="logEvent"
-                      v-on:share-started="logEvent"
-                      v-on:share-stopped="logEvent"
-                      @error="onError" />
-        </div>
-        <div class="row">
-          <div class="col-md-12 my-3">
-            <button type="button" class="btn btn-primary" @click="onJoin">Join</button>
-            <button type="button" class="btn btn-primary" @click="onLeave">Leave</button>
-            <button type="button" class="btn btn-primary" @click="onCapture">Capture Photo</button>
-            <button type="button" class="btn btn-primary" @click="onShareScreen">Share Screen</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <h2>Captured Image</h2>
-        <figure class="figure">
-          <img :src="img" class="img-responsive" />
-        </figure>
-      </div>
-    </div>
+  <div>
+    <q-btn
+    @click="open"
+    label="Open or Join Room"/>
+    <q-btn
+    @click="close"
+    label="Close"/>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-Vue.component()
+import * as RTCMultiConnection from 'rtcmulticonnection'
+export default {
+  data () {
+    return {
+      connection: new RTCMultiConnection()
+    }
+  },
+  methods: {
+    open () {
+      // this line is VERY_important
+      this.connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/'
+      this.connection.session = {
+        audio: true,
+        video: true
+      }
+      this.connection.openOrJoin('your-room-id')
+    },
+    close () {
+      this.connection.getAllParticipants().forEach(function (pid) {
+        this.connection.disconnectWith(pid)
+      })
+      // stop all local cameras
+      this.connection.attachStreams.forEach(function (localStream) {
+        localStream.stop()
+      })
+
+      // close socket.io connection
+      this.connection.closeSocket()
+    }
+  },
+  mounted () {
+
+  }
+}
+
 </script>
+
+<style>
+  video {
+    width: 40%;
+    border-radius: 15px;
+  }
+</style>
